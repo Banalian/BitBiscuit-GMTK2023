c9bc99ada4360b8c3109c05fa354eb90246509de
AdamWizard <48995051+AdamWizard@users.noreply.github.com>
2023-07-08 19:30:15 -0400

    Feat(GridBuilder behavior): made the builder skip the spots above the core
    the builder skips the 2 spots right above the core, we can also tell it here to put the core in the grid and what gameObject to use as the core.

--- Assets/Scripts/Grid/GridBuilder.cs
+++ Assets/Scripts/Grid/GridBuilder.cs
@@ -2,8 +2,11 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Numerics;
 using UnityEngine;
 using UnityEngine.Serialization;
+using Quaternion = UnityEngine.Quaternion;
+using Vector2 = UnityEngine.Vector2;
 
 namespace Grid
 {
@@ -20,14 +23,22 @@ namespace Grid
         private Vector2 spacing = Vector2.zero;
         
         [SerializeField] 
-        [Tooltip("Size of the grid, \nX: number of columns on wide rows\nY: number of rows")]
-        private Vector2 gridSize = new Vector2(3f,3f);
+        [Tooltip("Size of the grid, \nX: number of columns on narrow rows\nY: number of rows")]
+        private Vector2 gridSize = new Vector2(7,9);
+        
+        [SerializeField] 
+        [Tooltip("Position of the core, it will take up its own slots and the two slots immediately above it")]
+        private Vector2 corePosition = new Vector2(3,3);
         
         [SerializeField] 
         [Tooltip("The object that will be used to build the grid")]
         private GameObject buildingBlock;
         
+        [SerializeField] 
+        [Tooltip("The object that will be used as the core")]
+        private GameObject core;
 
+        
         private void Start()
         {
             BuildGrid();
@@ -36,18 +47,31 @@ namespace Grid
         private void BuildGrid()
         {
             Vector2 currentPosition = this.transform.position;
-            for (int i = 0; i < gridSize.y; i++)
+            for (int y = 0; y < gridSize.y; y++)
             {
-                for (int j = 0; j < gridSize.x + (i % 2 == 0 ? 0 : 1); j++)
+                for (int x = 0; x < gridSize.x + (y % 2 == 0 ? 0 : 1); x++)
                 {
-                    var newBrick = Instantiate(buildingBlock,currentPosition,Quaternion.identity,this.transform);
-                    newBrick.name += $" {i},{j}";
+                    if ((int)corePosition.x == y && (int)corePosition.y == x)
+                    {
+                        var coreBrick = Instantiate(core,currentPosition,Quaternion.identity,this.transform);
+                        coreBrick.name += $" {y},{x}";
+                    }
+                    else
+                    {
+                        //only if we are not on the spots above the core
+                        if (!(x == (int)corePosition.x-1 && y == (int)corePosition.y-1 ) &&
+                            !(x == (int)corePosition.x && y == (int)corePosition.y-1 ))
+                        {
+                            var newBrick = Instantiate(buildingBlock,currentPosition,Quaternion.identity,this.transform);
+                            newBrick.name += $" {y},{x}";
+                        }
+                    }
                     currentPosition.x += spacing.x + brickSize.x;
                 }
 
                 currentPosition.x = this.transform.position.x;
                 currentPosition.y -= spacing.y + brickSize.y;
-                currentPosition.x -= (i % 2 == 0 ? 1 : 0) * (brickSize.x+spacing.x) / 2;
+                currentPosition.x -= (y % 2 == 0 ? 1 : 0) * (brickSize.x+spacing.x) / 2;
             }
         }
     }
